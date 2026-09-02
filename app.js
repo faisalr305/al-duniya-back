b073234c1876e66423553830c1b7d1423ecd58e2
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-09-02 22:04:13 +0300

    fix

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 // imports
-const express = require("express")
-const app = express()
-const dotenv = require("dotenv").config()
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const morgan = require("morgan");
+const cors = require("cors");
 
 // Routes Import
-const authRoutes = require('./routes/auth.routes');
-const patientRoutes = require('./routes/patient.routes');
-const appointmentRoutes = require('./routes/appointment.routes');
-const paymentRoutes = require('./routes/payment.routes');
+const authRoutes = require("./routes/auth.routes");
+const patientRoutes = require("./routes/patient.routes");
+const appointmentRoutes = require("./routes/appointment.routes");
+const paymentRoutes = require("./routes/payment.routes");
 
 // Middleware
 app.use(
-    cors({
-        origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    })
+  cors({
+    origin: "https://al-duniya-appointment.netlify.app",
+    credentials: true,
+  }),
 );
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(morgan("dev"));
 
 // Routes
-app.use('/auth', authRoutes)
-app.use('/api/patients', patientRoutes)
-app.use('/api/appointments', appointmentRoutes)
-app.use('/api/payments', paymentRoutes)
+app.use("/auth", authRoutes);
+app.use("/api/patients", patientRoutes);
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api/payments", paymentRoutes);
 
-module.exports = app
+module.exports = app;
